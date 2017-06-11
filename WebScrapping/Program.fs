cde01ae9a8a4ab844ed1aca502c907a0e56ec860
Brian <brian593@live.com>
2017-06-11 13:28:51 -0500

    Agregar archivos de proyecto.

---
+++ WebScrapping/Program.fs
@@ -0,0 +1,20 @@
+﻿open System
+open System.IO
+open FSharp.Data
+
+type GoogleSearchResultPage = HtmlProvider<"https://www.google.com/search?q=brian593">
+
+let firstPage = GoogleSearchResultPage.Load("https://www.google.com/search?q=brian593")
+let secondPage = GoogleSearchResultPage.Load("https://www.google.com/search?q=brian593&start=10")
+
+firstPage.Html.Descendants()
+|> Seq.filter (fun n -> n.HasName("h3") && n.HasClass("r"))
+|> Seq.iter (fun n -> printfn "%s" (n.InnerText()))
+printfn "---Aqui comienza la segunda página ;) ----"
+secondPage.Html.Descendants()
+|> Seq.filter (fun n -> n.HasName("h3") && n.HasClass("r"))
+|> Seq.iter (fun n -> printfn "%s" (n.InnerText()))
+
+Console.ReadLine() |> ignore
+0
+
